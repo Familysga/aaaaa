a0e3962b30101d60461b1db4ddc12eca739fb6a6
Familysga <134228968+Familysga@users.noreply.github.com>
2024-04-30 23:40:20 +0300

    Update stats.py

--- AlexaMusic/plugins/tools/stats.py
+++ AlexaMusic/plugins/tools/stats.py
@@ -54,7 +54,9 @@ GSTATS_COMMAND = get_command("GSTATS_COMMAND")
 STATS_COMMAND = get_command("STATS_COMMAND")
 
 
-@app.on_message(filters.command(STATS_COMMAND) & filters.group & ~BANNED_USERS)
+@app.on_message(
+ filters.command(STATS_COMMAND,"")
+    & ~BANNED_USERS)
 @language
 async def stats_global(client, message: Message, _):
     upl = stats_buttons(_, True if message.from_user.id in SUDOERS else False)
