4f40d0e63529825aa3fe132a6396f8a42aa56795
Familysga <134228968+Familysga@users.noreply.github.com>
2024-04-30 23:19:04 +0300

    Update admins.py

--- AlexaMusic/utils/decorators/admins.py
+++ AlexaMusic/utils/decorators/admins.py
@@ -1,21 +1,8 @@
-# Copyright (C) 2024 by Alexa_Help @ Github, < https://github.com/TheTeamAlexa >
-# Subscribe On YT < Jankari Ki Duniya >. All rights reserved. © Alexa © Yukki.
-
-""""
-TheTeamAlexa is a project of Telegram bots with variety of purposes.
-Copyright (c) 2024 -present Team=Alexa <https://github.com/TheTeamAlexa>
-
-This program is free software: you can redistribute it and can modify
-as you want or you can collabe if you have new ideas.
-"""
-
-
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from pyrogram.enums import ChatType
-from config import adminlist
-from strings import get_string
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+
 from AlexaMusic import app
-from AlexaMusic.misc import SUDOERS
+from AlexaMusic.misc import SUDOERS, db
 from AlexaMusic.utils.database import (
     get_authuser_names,
     get_cmode,
@@ -25,43 +12,36 @@ from AlexaMusic.utils.database import (
     is_maintenance,
     is_nonadmin_chat,
 )
+from config import adminlist 
+from strings import get_string
 
 from ..formatters import int_to_alpha
 
-
+#Ar
 def AdminRightsCheck(mystic):
     async def wrapper(client, message):
         if await is_maintenance() is False:
-            if message.from_user.id not in SUDOERS:
-                return await message.reply_text(
-                    "» ʙᴏᴛ ɪs ᴜɴᴅᴇʀ ᴍᴀɪɴᴛᴇɴᴀɴᴄᴇ ғᴏʀ sᴏᴍᴇ ᴛɪᴍᴇ, ᴩʟᴇᴀsᴇ ᴠɪsɪᴛ sᴜᴩᴩᴏʀᴛ ᴄʜᴀᴛ ᴛᴏ ᴋɴᴏᴡ ᴛʜᴇ ʀᴇᴀsᴏɴ."
-                )
-        if await is_commanddelete_on(message.chat.id):
-            try:
-                await message.delete()
-            except:
-                pass
+            if message.from_user:
+                if message.from_user.id and message.from_user.id not in SUDOERS:
+                    return await message.reply_text(
+                        text=f"{app.mention} ɪs ᴜɴᴅᴇʀ ᴍᴀɪɴᴛᴇɴᴀɴᴄᴇ, ᴠɪsɪᴛ ғᴏʀ ᴋɴᴏᴡɪɴɢ ᴛʜᴇ ʀᴇᴀsᴏɴ.",
+                        disable_web_page_preview=True,
+                    )
+
+        try:
+            await message.delete()
+        except:
+            pass
+
         try:
             language = await get_lang(message.chat.id)
             _ = get_string(language)
         except:
             _ = get_string("en")
-        if message.sender_chat:
-            upl = InlineKeyboardMarkup(
-                [
-                    [
-                        InlineKeyboardButton(
-                            text="ʜᴏᴡ ᴛᴏ ғɪx ᴛʜɪs ?",
-                            callback_data="AnonymousAdmin",
-                        ),
-                    ]
-                ]
-            )
-            return await message.reply_text(_["general_4"], reply_markup=upl)
         if message.command[0][0] == "c":
             chat_id = await get_cmode(message.chat.id)
             if chat_id is None:
-                return await message.reply_text(_["setting_12"])
+                return await message.reply_text(_["setting_7"])
             try:
                 await app.get_chat(chat_id)
             except:
@@ -69,16 +49,56 @@ def AdminRightsCheck(mystic):
         else:
             chat_id = message.chat.id
         if not await is_active_chat(chat_id):
-            return await message.reply_text(_["general_6"])
+            return await message.reply_text(_["general_5"])
         is_non_admin = await is_nonadmin_chat(message.chat.id)
         if not is_non_admin:
-            if message.from_user.id not in SUDOERS:
-                admins = adminlist.get(message.chat.id)
-                if not admins:
-                    return await message.reply_text(_["admin_18"])
-                else:
-                    if message.from_user.id not in admins:
-                        return await message.reply_text(_["admin_19"])
+            if message.from_user:
+                if message.from_user.id and message.from_user.id not in SUDOERS:
+                    admins = adminlist.get(message.chat.id)
+                    if not admins:
+                        return await message.reply_text(_["admin_13"])
+                    else:
+                        if message.from_user.id not in admins:
+                            if await is_skipmode(message.chat.id):
+                                upvote = await get_upvote_count(chat_id)
+                                text = f"""<b>ᴀᴅᴍɪɴ ʀɪɢʜᴛs ɴᴇᴇᴅᴇᴅ</b>
+
+ʀᴇғʀᴇsʜ ᴀᴅᴍɪɴ ᴄᴀᴄʜᴇ ᴠɪᴀ : ريلود
+
+» {upvote} ᴠᴏᴛᴇs ɴᴇᴇᴅᴇᴅ ғᴏʀ ᴘᴇʀғᴏʀᴍɪɴɢ ᴛʜɪs ᴀᴄᴛɪᴏɴ."""
+
+                                command = message.command[0]
+                                if command[0] == "c":
+                                    command = command[1:]
+                                if command == "speed":
+                                    return await message.reply_text(_["admin_14"])
+                                MODE = command.title()
+                                upl = InlineKeyboardMarkup(
+                                    [
+                                        [
+                                            InlineKeyboardButton(
+                                                text="تصويت",
+                                                callback_data=f"ADMIN  UpVote|{chat_id}_{MODE}",
+                                            ),
+                                        ]
+                                    ]
+                                )
+                                if item in my_list:
+                                    [chat_id] = {}
+                                try:
+                                    vidid = db[chat_id][0]["vidid"]
+                                    file = db[chat_id][0]["file"]
+                                except:
+                                    return await message.reply_text(_["admin_14"])
+                                senn = await message.reply_text(text, reply_markup=upl)
+                                [chat_id][senn.id] = {
+                                    "vidid": vidid,
+                                    "file": file,
+                                }
+                                return
+                            else:
+                                return await message.reply_text(_["admin_14"])
+
         return await mystic(client, message, _, chat_id)
 
     return wrapper
@@ -87,41 +107,32 @@ def AdminRightsCheck(mystic):
 def AdminActual(mystic):
     async def wrapper(client, message):
         if await is_maintenance() is False:
-            if message.from_user.id not in SUDOERS:
-                return await message.reply_text(
-                    "» ʙᴏᴛ ɪs ᴜɴᴅᴇʀ ᴍᴀɪɴᴛᴇɴᴀɴᴄᴇ ғᴏʀ sᴏᴍᴇ ᴛɪᴍᴇ, ᴩʟᴇᴀsᴇ ᴠɪsɪᴛ sᴜᴩᴩᴏʀᴛ ᴄʜᴀᴛ ᴛᴏ ᴋɴᴏᴡ ᴛʜᴇ ʀᴇᴀsᴏɴ."
-                )
-        if await is_commanddelete_on(message.chat.id):
-            try:
-                await message.delete()
-            except:
-                pass
+            if message.from_user:
+                if message.from_user.id and message.from_user.id not in SUDOERS:
+                    return await message.reply_text(
+                        text=f"{app.mention} ɪs ᴜɴᴅᴇʀ ᴍᴀɪɴᴛᴇɴᴀɴᴄᴇ, ᴠɪsɪᴛ <a href={SUPPORT_CHAT}>sᴜᴘᴘᴏʀᴛ ᴄʜᴀᴛ</a> ғᴏʀ ᴋɴᴏᴡɪɴɢ ᴛʜᴇ ʀᴇᴀsᴏɴ.",
+                        disable_web_page_preview=True,
+                    )
+
+        try:
+            await message.delete()
+        except:
+            pass
+
         try:
             language = await get_lang(message.chat.id)
             _ = get_string(language)
         except:
             _ = get_string("en")
-        if message.sender_chat:
-            upl = InlineKeyboardMarkup(
-                [
-                    [
-                        InlineKeyboardButton(
-                            text="ʜᴏᴡ ᴛᴏ ғɪx ᴛʜɪs ?",
-                            callback_data="AnonymousAdmin",
-                        ),
-                    ]
-                ]
-            )
-            return await message.reply_text(_["general_4"], reply_markup=upl)
-        if message.from_user.id not in SUDOERS:
+        if message.from_user.id and message.from_user.id not in SUDOERS:
             try:
-                member = await app.get_chat_member(
-                    message.chat.id, message.from_user.id
-                )
+                member = (
+                    await app.get_chat_member(message.chat.id, message.from_user.id)
+                ).privileges
             except:
                 return
-            if not member.privileges.can_manage_video_chats:
-                return await message.reply(_["general_5"])
+            if not member.can_manage_video_chats:
+                return await message.reply(_["general_4"])
         return await mystic(client, message, _)
 
     return wrapper
@@ -132,7 +143,7 @@ def ActualAdminCB(mystic):
         if await is_maintenance() is False:
             if CallbackQuery.from_user.id not in SUDOERS:
                 return await CallbackQuery.answer(
-                    "» ʙᴏᴛ ɪs ᴜɴᴅᴇʀ ᴍᴀɪɴᴛᴇɴᴀɴᴄᴇ ғᴏʀ sᴏᴍᴇ ᴛɪᴍᴇ, ᴩʟᴇᴀsᴇ ᴠɪsɪᴛ sᴜᴩᴩᴏʀᴛ ᴄʜᴀᴛ ᴛᴏ ᴋɴᴏᴡ ᴛʜᴇ ʀᴇᴀsᴏɴ.",
+                    f"{app.mention} ɪs ᴜɴᴅᴇʀ ᴍᴀɪɴᴛᴇɴᴀɴᴄᴇ, ᴠɪsɪᴛ sᴜᴘᴘᴏʀᴛ ᴄʜᴀᴛ ғᴏʀ ᴋɴᴏᴡɪɴɢ ᴛʜᴇ ʀᴇᴀsᴏɴ.",
                     show_alert=True,
                 )
         try:
@@ -145,20 +156,22 @@ def ActualAdminCB(mystic):
         is_non_admin = await is_nonadmin_chat(CallbackQuery.message.chat.id)
         if not is_non_admin:
             try:
-                a = await app.get_chat_member(
-                    CallbackQuery.message.chat.id,
-                    CallbackQuery.from_user.id,
-                )
+                a = (
+                    await app.get_chat_member(
+                        CallbackQuery.message.chat.id,
+                        CallbackQuery.from_user.id,
+                    )
+                ).privileges
             except:
-                return await CallbackQuery.answer(_["general_5"], show_alert=True)
-            if not a.privileges.can_manage_video_chats:
+                return await CallbackQuery.answer(_["general_4"], show_alert=True)
+            if not a.can_manage_video_chats:
                 if CallbackQuery.from_user.id not in SUDOERS:
                     token = await int_to_alpha(CallbackQuery.from_user.id)
                     _check = await get_authuser_names(CallbackQuery.from_user.id)
                     if token not in _check:
                         try:
                             return await CallbackQuery.answer(
-                                _["general_5"],
+                                _["general_4"],
                                 show_alert=True,
                             )
                         except:
